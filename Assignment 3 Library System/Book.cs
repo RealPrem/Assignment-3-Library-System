d9710c27f9842447f28cfee15490ff6aac633a3c
prem <premplayz@gmail.com>
2021-01-14 14:54:28 +0700

    Add project files.

---
+++ Assignment 3 Library System/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3_Library_System
+{
+    class Book
+    {
+        private string Title;
+        private string Author;
+        private int Pages;
+        private bool Borrowed;
+        public static int NumberOfBooks;
+
+        public Book(string Title, string Author, int Pages)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Pages = Pages;
+            this.Borrowed = false;
+            NumberOfBooks = NumberOfBooks + 1;
+        }
+        public string GetTitle()
+        {
+            return Title;
+        }
+        public string GetAuthor()
+        {
+            return Author;
+        }
+        public int GetPages()
+        {
+            return Pages;
+        }
+        public bool isBorrowed()
+        {
+            return Borrowed;
+        }
+        public void BorrowBook()
+        {
+            if (Borrowed == false)
+            {
+                Borrowed = true;
+            }
+            else
+            {
+                Console.WriteLine(Title + "is not available");
+            }
+        }
+        public void ReturnBook()
+        {
+            if (Borrowed == true)
+            {
+                Borrowed = false;
+            }
+            else
+            {
+                Console.WriteLine(Title + " cannot be returned because not borrowed");
+            }
+        }
+        public int GetNumberOfBooks()
+        {
+            return NumberOfBooks;
+        }
+    }
+}
---
+++ Assignment 3 Library System/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_3_Library_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book harryPotter = new Book("Harry Potter", "Whoever", 100);
+            Console.WriteLine(Book.NumberOfBooks);
+            Book TheBible = new Book("The Bible", "God", 1000);
+            Console.WriteLine(Book.NumberOfBooks);
+            Book OneNineEightFour = new Book("1984", "Me", 200);
+            Console.WriteLine(Book.NumberOfBooks);
+
+            Book harpyp = new Book("HP", "JK", 100);
+            harpyp = null;
+            Console.WriteLine(harpyp.GetTitle());
+        }
+    }
+}
